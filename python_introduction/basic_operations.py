d2117408852955f9ea871169782c7a3d5b1e3e78
Foster Dwomoh <kdwomoh0269@gmail.com>
2024-06-21 15:14:20 -0700

    basic_operations.py

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,7 +1,7 @@
 number1 =10
 number2 =5
 result =number1 + number2
-result = number1 - number2
+result =number1 - number2
 result =number1 * number2
 print('sum of number1' and 'number2' is result)
 print('difference of number1' and 'number2' is result)
