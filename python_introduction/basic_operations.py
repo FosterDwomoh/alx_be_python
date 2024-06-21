3a9d4bee9e284e669afac04d50e8ffcb106613c9
Foster Dwomoh <kdwomoh0269@gmail.com>
2024-06-21 14:26:35 -0700

    basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+result = number1 + number2
+result = number1 - number2
+result = number1 * number2
+print('sum of number1' and 'number2' is result)
+print('difference of number1' and 'number2' is result)
+print('product of number1' and 'number2' is result)
