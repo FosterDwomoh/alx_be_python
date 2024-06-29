3b42076aedba9099078951d1882bd6d7610d32d1
Foster Dwomoh <kdwomoh0269@gmail.com>
2024-06-29 14:41:41 -0700

    match_case_calculator.py

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,25 @@
+num1 = float(input("enter the first number: "))
+num2 = float(input("enter the second number:"))
+
+# prompt the user to choose the operation
+operation = input("choose the operation (+, *, -, /): ")
+result = 0
+match operation:
+    case '+':
+        result = num1 + num2
+    case '-':
+        result = num1 - num2
+    case '*':
+        result = num1 * num2
+    case '/':
+        result = num1 / num2
+        if num2 == 0:
+            print("error: division by zero not allowed." )
+        else:
+            result = num1 / num2
+        case_:
+            print(f"error: '{operation}' is not a valid operation.")
+        if operation == '/' and num2 == 0:
+            pass
+        else:
+            print(f"the result is {result}")
