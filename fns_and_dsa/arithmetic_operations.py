d9a8564f3a9ccc5ec30d8f072624652517509473
Foster Dwomoh <kdwomoh0269@gmail.com>
2024-07-06 08:43:16 -0700

    arithmethic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,8 +1,8 @@
 from arithmetic_operations import perform_operation
 def main:
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
-    operation = input("Enter the operation(add/substract/multiply/divide): ")
+    num1= float(input("Enter the first number: "))
+    num2= float(input("Enter the second number: "))
+    operation= input("Enter the operation(add/substract/multiply/divide): ")
     result = perform_operation(num1, num2, operation)
     if isinstance(result,float):
         print(f"Result of {operation} operation:{result} ")
