80aa1ed8dc62d809472e805eec1168dff462c160
Foster Dwomoh <kdwomoh0269@gmail.com>
2024-07-07 15:12:07 -0700

    arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,5 +1,5 @@
 from arithmetic_operations import perform_operation
-def main:
+def perform_operation(num1, num2, operation):
     num1= float(input("Enter the first number: "))
     num2= float(input("Enter the second number: "))
     operation = input("Enter the operation(add, substract, multiply, divide): ").strip().lower()
