7607673b6a0825eff9b17a1de143f3ef3381d9b5
Foster Dwomoh <kdwomoh0269@gmail.com>
2024-07-14 08:32:56 -0700

    bank_account.py

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,14 @@
+# bank_account.py
+class BankAccount:
+    def_init_(self, initial_balance = 0):
+        self.account_balance = initial_balance
+        def deposit(self,amount):
+            self.amount_balance += amount
+        def withdraw(self, amount):
+            if self.account_balance >=amount:
+                self.account_balance -= amount
+                return True
+            selse:
+                return False
+            def display_balance(self):
+                print(f"Current Balance: ${self.account_balance:.2f}")
