6d79e2719721bfbb94303a7b4d61aa554feebd0a
Foster Dwomoh <kdwomoh0269@gmail.com>
2024-07-14 09:05:06 -0700

    bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -2,9 +2,9 @@
 class BankAccount:
     def __init__(self, initial_balance=0):
         self.account_balance = initial_balance
-        def deposit(self, amount):
+        def deposit(amount):
             self.amount_balance += amount
-        def withdraw(self, amount):
+        def withdraw(amount):
             if self.account_balance >=amount:
                 self.account_balance -= amount
                 return True
