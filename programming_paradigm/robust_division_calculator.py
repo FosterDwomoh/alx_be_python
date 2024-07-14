8d14ffc46e373e4f492897524744003da5c736f8
Foster Dwomoh <kdwomoh0269@gmail.com>
2024-07-14 09:33:28 -0700

    robust_division_calculator.py

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,13 +1,11 @@
-# robust_division_calculator.py
-import sys
-from robust_division_calculator import safe_divide
-def main():
-    if len(sys.argv)!=3:
-        print("usage:python main.py <numerator> <denominator>")
-        sys.exit(1)
-        numerator = sys.argv[1]
-        denominator = sys.argv[2]
-        result = safe_divide(numerator, denominator)
-        print(result)
-        if __name__ == " __main__":
-            main()
+# robust_division_calculator
+def saf_divide(numerator, denominator):
+    if denominator == 0:
+        return "Error:Division by Zero is not allowed."
+    try:
+        num = float(numerator)
+        denom = float(denominator)
+    except ValueError:
+        return "Error: Please provide numeric input."
+    result = num/denom
+    return result
