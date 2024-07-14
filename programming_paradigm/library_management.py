5412f90802503bd878d0a88c937b37d321710c25
Foster Dwomoh <kdwomoh0269@gmail.com>
2024-07-14 16:30:46 -0700

    library_management.py

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -31,22 +31,22 @@ class Book:
                 return True
             else:
                 return False
-            class library:
-                def __init__(self):
-                    self_books = []
-                    def add_book(self, book):
-                        self._books.append(book)
-                        def check_out_book(self, title):
-                            for book in self._books:
-                                if book.title == title:
-                                    return False
-                                def return_book(self, title):
-                                    for book in self._books:
-                                        if book.title == title:
-                                            return False
-                                        def list_available_books(self):
-                                            available_books = [book.title + 'by' + book.author for book in self._books if not book._is_checked_out]
-                                            if available_books:
-                                                print(book)
+class library:
+    def __init__(self):
+        self_books = []
+        def add_book(self, book):
+            self._books.append(book)
+            def check_out_book(self, title):
+                for book in self._books:
+                    if book.title == title:
+                        return False
+                    def return_book(self, title):
+                        for book in self._books:
+                            if book.title == title:
+                                return False
+                            def list_available_books(self):
+                                available_books = [book.title + 'by' + book.author for book in self._books if not book._is_checked_out]
+                                if available_books:
+                                    print(book)
                                             else:
                                                 print('No available books in the library.')
