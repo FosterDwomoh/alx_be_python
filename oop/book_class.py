88f35c1083d74d0ace29bdcd53ded8295e1e9d4d
Foster Dwomoh <kdwomoh0269@gmail.com>
2024-07-20 17:13:55 -0700

    book_class.py

--- oop/book_class.py
+++ oop/book_class.py
@@ -3,11 +3,11 @@ class Book:
         self.title = title
         self.author = author
         self.year = year
-        del __del__(self):
+        def ___del__(self):
             print(f"Deleting {self.title}")
-            del __str__(self):
+            def __str__(self):
                 return f"{self.title} by {self.author}, published in {self.year}"
-            del __repr__(self):
+            def __repr__(self):
                 return f"Book('{self.title}', '{self.author}, published in {self.year}"
             if __name__ == "__main__"
             my_book = Book("1984" "George Orwell", 1949)
