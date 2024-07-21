dcc180e38814ac108bf5fc47cf4e0e39b2dbce89
Foster Dwomoh <kdwomoh0269@gmail.com>
2024-07-21 14:52:59 -0700

    polymorphism_demo.py

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,21 @@
+import math
+class shape:
+    def area(self):
+        raise NotImplementedError("subclasses should implement this method.")
+class Rectangle(shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+    def arae(self):
+        return self.length * self.width
+    class Circle(shape):
+        def __init__(self, radius):
+            self.radius = radius
+        def area(self):
+            return math.pi * self.radius ** 2
+        if __name__ == " __main__ ":
+            rectangle = Rectangle(10, 5)
+            circle = Circle(7)
+            print(f"The Area of the Rectangle is: {rectangle.area()}")
+            print(f"The Area of the Circle is: {circle.area()}")
+
