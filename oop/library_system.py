9ce012250dd225ea957245eca083d3074808d0b2
Foster Dwomoh <kdwomoh0269@gmail.com>
2024-07-21 14:24:51 -0700

    library_system.py

--- oop/library_system.py
+++ oop/library_system.py
@@ -29,13 +29,13 @@ class library:
             print(f"PrintBook: {book.title} by {book.author}, page count: {book.page_count}")
         elif isinstance(book, Book):
             print(f"Book: {book.title} by {book.author}")
-            if __name__ == " __main__":
-                classic_book = Book("Pride and Prejudice", "Jane Austen")
-                digital_novel = EBook("Snow Crash", "Neal Stephenson", 500)
-                paper_novel = PrintBook("The Catcher in the Rye", "J.D. Salinger", 234)
-                my_library.add_book(classic_book)
-                my_library.add_book(digital_novel)
-                my_library.add_book(paper_novel)
-                my_library.list_books()
-                if __name__=="__main__":
-                    main()
+    def main:
+        classic_book = Book("Pride and Prejudice", "Jane Austen")
+        digital_novel = EBook("Snow Crash", "Neal Stephenson", 500)
+        paper_novel = PrintBook("The Catcher in the Rye", "J.D. Salinger", 234)
+        my_library.add_book(classic_book)
+        my_library.add_book(digital_novel)
+        my_library.add_book(paper_novel)
+        my_library.list_books()
+    if __name__=="__main__":
+        main()
